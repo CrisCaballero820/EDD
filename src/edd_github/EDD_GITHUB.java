979de26263953eb8184edcba5d9755db7b253cce
CrisCaballero820 <99421785+CrisCaballero820@users.noreply.github.com>
2022-11-17 13:50:55 -0700

    Update EDD_GITHUB.java

--- src/edd_github/EDD_GITHUB.java
+++ src/edd_github/EDD_GITHUB.java
@@ -6,7 +6,8 @@ public class EDD_GITHUB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    System.out.println("Hola Mundo");
+
     }
     
 }
